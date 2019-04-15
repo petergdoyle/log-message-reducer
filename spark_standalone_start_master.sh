d4f564754d54c6edd16359f2ddae6decb0fb9fc7
petergdoyle <peter.doyle@travelport.com>
2019-04-15 05:32:49 -0600

    fixed up inconsistencies

--- spark_standalone_start_master.sh
+++ spark_standalone_start_master.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-echo "removing old log file..."
+echo "removing old master log file..."
 sleep 1
 log_file="$SPARK_HOME/logs/spark-vagrant-org.apache.spark.deploy.master.Master-1-log-message-reducer.cleverfishsoftware.com.out"
 rm -v $log_file
@@ -11,6 +11,6 @@ echo "starting master..."
 sleep 1
 $SPARK_HOME/sbin/start-master.sh
 
-echo "tailing on log for 5s..."
+echo "tailing on master log for 5s..."
 sleep 1
 timeout 5s tail -f $log_file
--- spark_standalone_start_worker.sh
+++ spark_standalone_start_worker.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-echo "removing old log file..."
+echo "removing old worker log file..."
 sleep 1
 log_file="$SPARK_HOME/logs/spark-vagrant-org.apache.spark.deploy.worker.Worker-1-log-message-reducer.cleverfishsoftware.com.out"
 rm -v $log_file
 sleep 1
 
-echo "starting master..."
+echo "starting worker..."
 sleep 1
 $SPARK_HOME/sbin/start-slave.sh spark://log-message-reducer.cleverfishsoftware.com:7077
 
-echo "tailing on log for 5s..."
+echo "tailing on worker log for 5s..."
 sleep 1
 timeout 5s tail -f $log_file

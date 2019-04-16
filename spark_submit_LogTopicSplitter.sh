a10f7ac15722b7c64baa40b3708b36b035dc5e1d
petergdoyle <peter.doyle@travelport.com>
2019-04-16 09:11:30 -0600

    spark_submit_LogTopicSplitter.sh

--- spark_submit_LogTopicSplitter.sh
+++ spark_submit_LogTopicSplitter.sh
@@ -8,27 +8,26 @@ driver_java_options_quiet='--driver-java-options "-Dlog4j.configuration=file:///
 driver_java_options_default=''
 driver_java_options=$driver_java_options_quiet
 
-# System.err.println("Usage: LogTopicSplitter <brokers> <consumerGroupId> <consumer-topic> <producerErrTopic> <producerOutTopic> <checkpointsDir> <outputsDir>\n"
-#         + "  <brokers> is a list of one or more Kafka brokers\n"
-#         + "  <consumerGroupId> is a consumer group name to consume from topics\n"
-#         + "  <consumerTopic> the topic to listen for mixed log messages\n"
-#         + "  <producerErrTopic> the topic to put error messages onto \n"
-#         + "  <producerOutTopic> the topic to put non-error messages onto \n"
-#         + "  <checkpointDir> the location for spark streaming checkpoints\n"
-#         + "  <outputsDir> the location for any console output\n"
-
 broker_list='localhost:9092'
 consumer_topic_name="logs"
 consumer_group_id="LogTopicSplitter-cg"
 producer_err_topic_name="logs-stderr"
 producer_out_topic_name="logs-stdout"
 checkpoint_dir="/spark/checkpoint"
-violations_dir="$PWD/violations"
-params="$broker_list $consumer_group_id $consumer_topic_name $producer_err_topic_name $producer_out_topic_name $checkpoint_dir $violations_dir"
+
+    # val brokers=args(0)
+    # val consumerGroupId=args(1)
+    # val consumerTopic=args(2)
+    # val producerErrTopic=args(3)
+    # val producerOutTopic=args(4)
+    # val checkpointDir=args(5)
+
+params="$broker_list $consumer_group_id $consumer_topic_name $producer_err_topic_name $producer_out_topic_name $checkpoint_dir"
+
 skip_build="$1"
 status=0
 if  [ "$skip_build" != "--skipBuild" ]; then
-  mvn -f log-message-reducer/pom.xml package
+  mvn --offline -f log-message-reducer/pom.xml package
   status=$?
 fi
 

33e5bd02b8af9c9b6a8e81f0712d678d8f165ce5
petergdoyle <peter.doyle@travelport.com>
2019-04-21 17:27:23 -0600

    part of that fix up to move cleanup into the teardown script

--- spark_standalone_start_worker.sh
+++ spark_standalone_start_worker.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-echo "removing old worker log file..."
-sleep 1
-log_file="$SPARK_HOME/logs/spark-vagrant-org.apache.spark.deploy.worker.Worker-1-log-message-reducer.cleverfishsoftware.com.out"
-rm -v $log_file
-sleep 1
-
 echo "starting worker..."
 sleep 1
 $SPARK_HOME/sbin/start-slave.sh spark://log-message-reducer.cleverfishsoftware.com:7077

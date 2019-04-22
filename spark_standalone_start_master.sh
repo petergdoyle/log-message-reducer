165c701a6a25973ed525297922d408e71ff8f396
petergdoyle <peter.doyle@travelport.com>
2019-04-22 13:09:50 -0600

    cleanup

--- spark_standalone_start_master.sh
+++ spark_standalone_start_master.sh
@@ -5,6 +5,7 @@ echo "starting master..."
 sleep 1
 $SPARK_HOME/sbin/start-master.sh
 
+log_file="$SPARK_HOME/logs/spark-vagrant-org.apache.spark.deploy.master.Master-1-log-message-reducer.cleverfishsoftware.com.out"
 echo "tailing on master log for 5s..."
 sleep 1
 timeout 5s tail -f $log_file
--- spark_standalone_start_worker.sh
+++ spark_standalone_start_worker.sh
@@ -4,6 +4,7 @@ echo "starting worker..."
 sleep 1
 $SPARK_HOME/sbin/start-slave.sh spark://log-message-reducer.cleverfishsoftware.com:7077
 
+log_file="$SPARK_HOME/logs/spark-vagrant-org.apache.spark.deploy.worker.Worker-1-log-message-reducer.cleverfishsoftware.com.out"
 echo "tailing on worker log for 5s..."
 sleep 1
 timeout 5s tail -f $log_file

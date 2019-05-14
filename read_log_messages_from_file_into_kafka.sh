2e7214a593c625f2d96b59ee60c1ccce4f750081
petergdoyle <peter.doyle@travelport.com>
2019-05-14 05:37:24 -0600

    added prompts to run this utility

--- read_log_messages_from_file_into_kafka.sh
+++ read_log_messages_from_file_into_kafka.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-class_name='com.cleverfishsoftware.utils.messagegenerator.RunKafkaMessageSenderFromFile'
+class_name='com.cleverfishsoftware.utils.messagegenerator.KafkaMessageSenderFromFile'
 jar_name='log-message-generator/target/log-message-generator-1.0-SNAPSHOT.jar'
-log_file_name="$PWD/logs/log-message-generator.log"
 
-# mvn -f log-message-generator/pom.xml clean install \
-# && \
-java -cp $jar_name $class_name $log_file_name
+
+log_file_name="$PWD/logs/app.log"
+read -e -p "Enter log file name: " -i "$log_file_name" log_file_name
+
+mvn -f log-message-generator/pom.xml package
+
+cmd="java -cp $jar_name $class_name $log_file_name"
+echo "$cmd"
+eval "$cmd"
